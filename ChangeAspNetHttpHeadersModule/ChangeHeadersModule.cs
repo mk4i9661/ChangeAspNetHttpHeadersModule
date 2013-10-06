65899b83ddbdd1268b3cf0dca29fa2957224d33c
Dmitry <mk4i9661@hotmail.com>
2013-10-06 21:16:10 +0400

    -

---
+++ ChangeAspNetHttpHeadersModule/ChangeHeadersModule.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web;
+
+namespace ChangeAspNetHttpHeadersModule
+{
+    public class ChangeHeadersModule : IHttpModule 
+    {
+        public void Dispose() {
+            
+        }
+
+        public void Init(HttpApplication context) {
+            context.PreSendRequestHeaders += context_PreSendRequestHeaders;
+        }
+
+        void context_PreSendRequestHeaders(object sender, EventArgs e) {
+            HttpContext.Current.Response.Headers.Set("Server", "B-baka, it's not like I changed this header especially for you or anything!");
+            HttpContext.Current.Response.Headers.Remove("x-aspnet-version");
+            HttpContext.Current.Response.Headers.Remove("x-aspnetmvc-version");
+        }
+    }
+}
